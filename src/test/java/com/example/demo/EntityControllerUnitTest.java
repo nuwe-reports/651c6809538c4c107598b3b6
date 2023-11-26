9a13c57cd282f1012e2cf8647396b6cb0d9e0dc7
albertocstll70 <albertocstll70@gmail.com>
2023-11-26 23:40:33 +0100

    solucion#alberto

--- src/test/java/com/example/demo/EntityControllerUnitTest.java
+++ src/test/java/com/example/demo/EntityControllerUnitTest.java
@@ -21,6 +21,7 @@ import java.time.format.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -40,62 +41,224 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * Make sure to be as exhaustive as possible. Coverage is checked ;)
  */
 
+
+
 @WebMvcTest(DoctorController.class)
-class DoctorControllerUnitTest{
+class DoctorControllerUnitTest {
 
-    @MockBean
-    private DoctorRepository doctorRepository;
+	@MockBean
+	private DoctorRepository doctorRepository;
 
-    @Autowired 
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-    @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
-    }
+	@Test
+	void shouldGetAllDoctors() throws Exception {
+		List<Doctor> doctors = Arrays.asList(new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com"),
+				new Doctor("Hector", "De la Rosa", 52, "Hdelarosa@hospital.com"));
+
+		when(doctorRepository.findAll()).thenReturn(doctors);
+
+		mockMvc.perform(get("/api/doctors")).andExpect(status().isOk())
+				.andExpect(content().json(objectMapper.writeValueAsString(doctors)));
+	}
+
+	@Test
+	void shouldGetDoctorById() throws Exception {
+		Doctor doctor = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+
+		when(doctorRepository.findById(1L)).thenReturn(Optional.of(doctor));
+
+		mockMvc.perform(get("/api/doctors/1")).andExpect(status().isOk())
+				.andExpect(content().json(objectMapper.writeValueAsString(doctor)));
+	}
+
+	@Test
+	void shouldCreateDoctor() throws Exception {
+		Doctor doctorToCreate = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+
+		mockMvc.perform(post("/api/doctor").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(doctorToCreate))).andExpect(status().isCreated());
+	}
+
+	@Test
+	void shouldDeleteDoctorById() throws Exception {
+
+		Doctor existingDoctor = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+
+		when(doctorRepository.findById(1L)).thenReturn(Optional.of(existingDoctor));
+
+		mockMvc.perform(delete("/api/doctors/1")).andExpect(status().isOk());
+	}
+
+	@Test
+	void shouldDeleteAllDoctors() throws Exception {
+		mockMvc.perform(delete("/api/doctors")).andExpect(status().isOk());
+	}
 }
 
-
 @WebMvcTest(PatientController.class)
-class PatientControllerUnitTest{
+class PatientControllerUnitTest {
 
     @MockBean
     private PatientRepository patientRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    void shouldGetAllPatients() throws Exception {
+        List<Patient> patients = Arrays.asList(new Patient("Juana", "Sosa", 53, "jsosa@hospital.com"),
+        		new Patient("Albert", "Triviño", 23, "Atrivino@hospital.com")
+        );
+
+        when(patientRepository.findAll()).thenReturn(patients);
+
+        mockMvc.perform(get("/api/patients"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(patients)));
     }
 
-}
+    @Test
+    void shouldGetPatientById() throws Exception {
+        Patient patient = new Patient("Juana", "Sosa", 53, "jsosa@hospital.com");
 
+        when(patientRepository.findById(1L)).thenReturn(Optional.of(patient));
+
+        mockMvc.perform(get("/api/patients/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(patient)));
+    }
+
+    @Test
+    void shouldCreatePatient() throws Exception {
+        Patient patientToCreate = new Patient("Alice", "Smith", 25, "alice.smith@example.com");
+
+        mockMvc.perform(post("/api/patient")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(patientToCreate)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void shouldDeletePatientById() throws Exception {
+       
+        Patient existingPatient = new Patient("Juana", "Sosa", 53, "jsosa@hospital.com");
+
+       
+        when(patientRepository.findById(1L)).thenReturn(Optional.of(existingPatient));
+
+        mockMvc.perform(delete("/api/patients/1"))
+                .andExpect(status().isOk());
+        }
+
+    @Test
+    void shouldDeleteAllPatients() throws Exception {
+        mockMvc.perform(delete("/api/patients"))
+                .andExpect(status().isOk());
+        }
+}
+/*
 @WebMvcTest(RoomController.class)
-class RoomControllerUnitTest{
+class RoomControllerUnitTest {
+
+	@MockBean
+	private RoomRepository roomRepository;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	void shouldCreateRoom() throws Exception {
+		Room room = new Room("gynecology");
+		mockMvc.perform(post("/api/room").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(room))).andExpect(status().isCreated());
+	}
+
+	@Test
+	void shouldGetRoomById() throws Exception {
+		Room room = new Room("gynecology");
+
+		Optional<Room> opt = Optional.of(room);
+
+		assertThat(opt).isPresent();
+		assertThat(opt.get().getRoomName()).isEqualTo(room.getRoomName());
+		assertThat(room.getRoomName()).isEqualTo("gynecology");
+
+		when(roomRepository.findByRoomName(room.getRoomName())).thenReturn(opt);
+		mockMvc.perform(get("/api/rooms/" + room.getRoomName())).andExpect(status().isOk());
+
+	}*/
+@WebMvcTest(RoomController.class)
+class RoomControllerUnitTest {
 
     @MockBean
     private RoomRepository roomRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+    void shouldGetAllRooms() throws Exception {
+        List<Room> rooms = Arrays.asList( new Room("gynecology"), new Room("dermatology"));
+
+        when(roomRepository.findAll()).thenReturn(rooms);
+
+        mockMvc.perform(get("/api/rooms"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(rooms)));
     }
 
+    @Test
+    void shouldGetRoomByRoomName() throws Exception {
+        Room room = new Room("gynecology");
+
+        when(roomRepository.findByRoomName("gynecology")).thenReturn(Optional.of(room));
+
+        mockMvc.perform(get("/api/rooms/gynecology"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(room)));
+    }
+
+    @Test
+    void shouldCreateRoom() throws Exception {
+        Room roomToCreate = new Room("dermatology");
+
+        mockMvc.perform(post("/api/room")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(roomToCreate)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void shouldDeleteRoomByRoomName() throws Exception {
+       
+        Room existingRoom = new Room("dermatology");
+
+        
+        when(roomRepository.findByRoomName("dermatology")).thenReturn(Optional.of(existingRoom));
+
+        mockMvc.perform(delete("/api/rooms/dermatology"))
+                .andExpect(status().isOk());
+        }
+
+    @Test
+    void shouldDeleteAllRooms() throws Exception {
+        mockMvc.perform(delete("/api/rooms"))
+                .andExpect(status().isOk());
+        }
 }
+
+
--- src/test/java/com/example/demo/EntityUnitTest.java
+++ src/test/java/com/example/demo/EntityUnitTest.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -37,14 +39,275 @@ class EntityUnitTest {
     private Appointment a2;
     private Appointment a3;
 
-    @Test
-    void this_is_a_test(){
-        // DELETE THIS TEST
-        assertThat(false).isEqualTo(true);
-    }
+   
 
     /** TODO
      * Implement tests for each Entity class: Doctor, Patient, Room and Appointment.
      * Make sure you are as exhaustive as possible. Coverage is checked ;)
      */
+    
+    @Test
+	void testDoctorEntityPersistence() {
+		d1 = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+
+		entityManager.persist(d1);
+
+		Doctor retrievedDoctor = entityManager.find(Doctor.class, d1.getId());
+
+		assertThat(retrievedDoctor).isNotNull();
+		assertThat(retrievedDoctor.getFirstName()).isEqualTo("Marcos");
+		assertThat(retrievedDoctor.getLastName()).isEqualTo("Corporan");
+		assertThat(retrievedDoctor.getAge()).isEqualTo(28);
+		assertThat(retrievedDoctor.getEmail()).isEqualTo("Mcorporan@hospital.com");
+	}
+
+	@Test
+	void testDoctorEntityNull() {
+		d1 = new Doctor();
+		entityManager.persist(d1);
+		Doctor retrievedDoctorNull = entityManager.find(Doctor.class, d1.getId());
+
+		assertThat(retrievedDoctorNull).isNotNull();
+		assertThat(retrievedDoctorNull.getFirstName()).isNull();
+		assertThat(retrievedDoctorNull.getLastName()).isNull();
+		assertThat(retrievedDoctorNull.getAge()).isEqualTo(0);
+		assertThat(retrievedDoctorNull.getEmail()).isNull();
+	}
+
+	// Patient
+	@Test
+	void testPatientEntityPersistence() {
+		p1 = new Patient("Juana", "Sosa", 53, "jsosa@xmail.com");
+		entityManager.persist(p1);
+
+		Patient retrievedPatient = entityManager.find(Patient.class, p1.getId());
+
+		assertThat(retrievedPatient).isNotNull();
+		assertThat(retrievedPatient.getFirstName()).isEqualTo("Juana");
+		assertThat(retrievedPatient.getLastName()).isEqualTo("Sosa");
+		assertThat(retrievedPatient.getAge()).isEqualTo(53);
+		assertThat(retrievedPatient.getEmail()).isEqualTo("jsosa@xmail.com");
+	}
+
+	@Test
+	void testPatientEntityNull() {
+		p1 = new Patient();
+		entityManager.persist(p1);
+		Patient retrievedPatientNull = entityManager.find(Patient.class, p1.getId());
+
+		assertThat(retrievedPatientNull).isNotNull();
+		assertThat(retrievedPatientNull.getFirstName()).isNull();
+		assertThat(retrievedPatientNull.getLastName()).isNull();
+		assertThat(retrievedPatientNull.getAge()).isEqualTo(0);
+		assertThat(retrievedPatientNull.getEmail()).isNull();
+	}
+
+	// Room
+	@Test
+	void testRoomEntityPersistence() {
+		r1 = new Room("gynecology");
+
+		entityManager.persist(r1);
+
+		Room retrievedRoom = entityManager.find(Room.class, r1.getRoomName());
+
+		assertThat(retrievedRoom).isNotNull();
+		assertThat(retrievedRoom.getRoomName()).isEqualTo("gynecology");
+	}
+
+	// Appointment
+	@Test
+	void testAppointmentEntityPersistence() {
+		d1 = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+		p1 = new Patient("Juana", "Sosa", 53, "jsosa@xmail.com");
+		r1 = new Room("gynecology");
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+
+		LocalDateTime startsAt = LocalDateTime.parse("19:30 22/10/2023", formatter);
+		LocalDateTime finishesAt = LocalDateTime.parse("20:30 22/10/2023", formatter);
+
+		a1 = new Appointment(p1, d1, r1, startsAt, finishesAt);
+
+		entityManager.persist(a1);
+
+		Appointment retrievedAppointment = entityManager.find(Appointment.class, a1.getId());
+
+		assertThat(retrievedAppointment).isNotNull();
+		assertThat(retrievedAppointment.getPatient()).isEqualTo(p1);
+		assertThat(retrievedAppointment.getDoctor()).isEqualTo(d1);
+		assertThat(retrievedAppointment.getRoom()).isEqualTo(r1);
+		assertThat(retrievedAppointment.getStartsAt()).isEqualTo(startsAt);
+		assertThat(retrievedAppointment.getFinishesAt()).isEqualTo(finishesAt);
+
+	}
+
+	@Test
+	void testAppointmentEntityPersistenceNull() {
+		a1 = new Appointment();
+
+		entityManager.persist(a1);
+
+		Appointment retrievedAppointment = entityManager.find(Appointment.class, a1.getId());
+
+		assertThat(retrievedAppointment).isNotNull();
+		assertThat(retrievedAppointment.getPatient()).isNull();
+		assertThat(retrievedAppointment.getDoctor()).isNull();
+		assertThat(retrievedAppointment.getRoom()).isNull();
+		assertThat(retrievedAppointment.getStartsAt()).isNull();
+		assertThat(retrievedAppointment.getFinishesAt()).isNull();
+
+	}
+
+	@Test
+	void testValidDateTimeFormat() {
+
+		// String validDateTimeString = "23:59 22/10/2023";
+		LocalDateTime validDateTime = LocalDateTime.of(2023, 10, 22, 23, 59);
+
+		Appointment appointment = new Appointment();
+		appointment.setStartsAt(validDateTime);
+		appointment.setFinishesAt(validDateTime);
+
+		LocalDateTime retrievedStartsAt = appointment.getStartsAt();
+		LocalDateTime retrievedFinishesAt = appointment.getFinishesAt();
+
+		assertThat(retrievedStartsAt).isNotNull();
+		assertThat(retrievedFinishesAt).isNotNull();
+		assertThat(retrievedStartsAt).isEqualTo(validDateTime);
+		assertThat(retrievedFinishesAt).isEqualTo(validDateTime);
+
+	}
+
+	@Test
+	void testAppointmentRelationship() {
+		d1 = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+		p1 = new Patient("Juana", "Sosa", 53, "jsosa@xmail.com");
+		r1 = new Room("gynecology");
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+
+		LocalDateTime startsAt = LocalDateTime.parse("19:30 22/10/2023", formatter);
+		LocalDateTime finishesAt = LocalDateTime.parse("20:30 22/10/2023", formatter);
+
+		a1 = new Appointment(p1, d1, r1, startsAt, finishesAt);
+
+		assertThat(a1).isNotNull();
+		assertThat(a1.getPatient()).isEqualTo(p1);
+		assertThat(a1.getDoctor()).isEqualTo(d1);
+		assertThat(a1.getRoom()).isEqualTo(r1);
+		assertThat(a1.getStartsAt()).isEqualTo(startsAt);
+		assertThat(a1.getFinishesAt()).isEqualTo(finishesAt);
+
+	}
+
+	@Test
+	void testAppointmentOverlapsCase1() {
+		// Case 1: A.starts == B.starts
+		
+		d1 = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+		p1 = new Patient("Juana", "Sosa", 53, "jsosa@xmail.com");		
+		r1 = new Room("gynecology");
+
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+
+		LocalDateTime startsAtA = LocalDateTime.parse("19:30 22/10/2023", formatter);
+		LocalDateTime finishesAtA = LocalDateTime.parse("20:30 22/10/2023", formatter);
+		LocalDateTime startsAtB = LocalDateTime.parse("19:30 22/10/2023", formatter);
+		LocalDateTime finishesAtB = LocalDateTime.parse("21:00 22/10/2023", formatter);
+
+		a1 = new Appointment(p1, d1, r1, startsAtA, finishesAtA);
+		a2 = new Appointment(p1, d1, r1, startsAtB, finishesAtB);
+
+		assertTrue(a1.overlaps(a2));
+	}
+
+	@Test
+	void testAppointmentOverlapsCase2() {
+		// Case 2: A.finishes == B.finishes
+		
+		d1 = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+		p1 = new Patient("Juana", "Sosa", 53, "jsosa@xmail.com");
+		r1 = new Room("gynecology");
+
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+
+		LocalDateTime startsAtA = LocalDateTime.parse("19:00 22/10/2023", formatter);
+		LocalDateTime finishesAtA = LocalDateTime.parse("20:30 22/10/2023", formatter);
+		LocalDateTime startsAtB = LocalDateTime.parse("19:30 22/10/2023", formatter);
+		LocalDateTime finishesAtB = LocalDateTime.parse("20:30 22/10/2023", formatter);
+
+		a1 = new Appointment(p1, d1, r1, startsAtA, finishesAtA);
+		a2 = new Appointment(p1, d1, r1, startsAtB, finishesAtB);
+
+		assertTrue(a1.overlaps(a2));
+
+	}
+
+	@Test
+	void testAppointmentOverlapsCase3() {
+		// Case 3: A.starts < B.finishes && B.finishes < A.finishes
+		
+		d1 = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+		p1 = new Patient("Juana", "Sosa", 53, "jsosa@xmail.com");
+		r1 = new Room("gynecology");
+
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+
+		LocalDateTime startsAtA = LocalDateTime.parse("19:30 22/10/2023", formatter);
+		LocalDateTime finishesAtA = LocalDateTime.parse("20:30 22/10/2023", formatter);
+		LocalDateTime startsAtB = LocalDateTime.parse("20:00 22/10/2023", formatter);
+		LocalDateTime finishesAtB = LocalDateTime.parse("20:15 22/10/2023", formatter);
+		
+		a1 = new Appointment(p1, d1, r1, startsAtA, finishesAtA);
+		a2 = new Appointment(p1, d1, r1, startsAtB, finishesAtB);
+		
+		assertTrue(a1.overlaps(a2));
+
+	}
+
+	@Test
+	void testAppointmentOverlapsCase4() {
+		 // Case 4: B.starts < A.starts && A.finishes < B.finishes
+		
+		d1 = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+		p1 = new Patient("Juana", "Sosa", 53, "jsosa@xmail.com");		
+		r1 = new Room("gynecology");
+
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+
+		LocalDateTime startsAtA = LocalDateTime.parse("19:30 22/10/2023", formatter);
+		LocalDateTime finishesAtA = LocalDateTime.parse("20:00 22/10/2023", formatter);
+		LocalDateTime startsAtB = LocalDateTime.parse("19:00 22/10/2023", formatter);
+		LocalDateTime finishesAtB = LocalDateTime.parse("19:45 22/10/2023", formatter);
+		
+		a1 = new Appointment(p1, d1, r1, startsAtA, finishesAtA);
+		a2 = new Appointment(p1, d1, r1, startsAtB, finishesAtB);
+		
+		assertTrue(a1.overlaps(a2));
+	}
+
+	@Test
+	void testAppointmentOverlaps() {
+		d1 = new Doctor("Marcos", "Corporan", 28, "Mcorporan@hospital.com");
+		p1 = new Patient("Juana", "Sosa", 53, "jsosa@xmail.com");		
+		r1 = new Room("gynecology");
+
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+
+		LocalDateTime startsAtA = LocalDateTime.parse("10:00 23/10/2023", formatter);
+		LocalDateTime finishesAtA = LocalDateTime.parse("10:30 23/10/2023", formatter);
+		LocalDateTime startsAtB = LocalDateTime.parse("11:30 23/10/2023", formatter);
+		LocalDateTime finishesAtB = LocalDateTime.parse("12:00 23/10/2023", formatter);
+		
+		a1 = new Appointment(p1, d1, r1, startsAtA, finishesAtA);
+		a2 = new Appointment(p1, d1, r1, startsAtB, finishesAtB);
+		
+		assertFalse(a1.overlaps(a2));
+	}
 }
